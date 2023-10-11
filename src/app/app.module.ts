d70de2c71fa6b61930d99fa38a880c68f255ec63
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-10-11 19:16:19 +0200

    parallax study

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { FooterComponent } from './footer/footer/footer.component';
+import { FooterComponent } from './home/footer/footer/footer.component';
+import { MainSectionComponent } from './home/main-section/main-section.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent],
+  declarations: [AppComponent, FooterComponent, MainSectionComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
